2f488f74291abd45e8b2e288aec6482e92b095da
neyaz <neyazmorshid@gmail.com>
2025-05-06 17:16:19 +0600

    jwt removed as it is not working

--- src/routes/fetchOdersDataRoute.js
+++ src/routes/fetchOdersDataRoute.js
@@ -8,15 +8,15 @@ const verifyToken = require('../Middleware/jwtMiddleware');
 
 const router = express.Router();
 
-router.get('/storeOrders/:storeId', verifyToken,async (req, res) => {
+router.get('/storeOrders/:storeId',async (req, res) => {
   const storeId = req.params.storeId;
   const cursor = req.query.cursor || null;
-  const requestedEmail = req.query.email;
+  // const requestedEmail = req.query.email;
   
-  // Verify that the authenticated user matches the requested email
-  if (requestedEmail && req.user.email !== requestedEmail) {
-    return res.status(403).json({ message: 'Forbidden access: Email mismatch' });
-  }
+  // // Verify that the authenticated user matches the requested email
+  // if (requestedEmail && req.user.email !== requestedEmail) {
+  //   return res.status(403).json({ message: 'Forbidden access: Email mismatch' });
+  // }
 
   try {
     const store = await StoreModel.findById(storeId);
